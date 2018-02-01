9314921176aadf3ce99004c5808c7570f4bc4985
wangtq <wangtq1990@126.com>
2018-02-01 16:31:27 +0800

    modify

--- shopping.py
+++ shopping.py
@@ -1,8 +1,11 @@
 #-*-coding:utf-8-*- 
 __author__ = 'Aaron2Study'
 '''
-exercise for study python
-a little shopping program
+python 学习
+购物小程序练习
+包含两级菜单，可以购买多件，
+允许多用户登录，能够保存用户账户， 
+能够查看购买记录，显示购物车列表
 '''
 
 product = [
@@ -19,7 +22,7 @@ if salary.isdigit():
     salary = int(salary)
     print("Welcome to my shopping mall with salary [%s]".center(50,'-') % salary)
 else:
-    print("Your input invaild type....")
+    print("Your input invalid type....")
 
 exit_flag = False
 while not exit_flag:
@@ -41,16 +44,28 @@ while not exit_flag:
             user_choice = input("Please choose the product that you want to buy[c for check, q for quit]:")
             if user_choice.isdigit():
                 user_choice = int(user_choice)
-                user_product = product[user_class][1][user_choice][0]
-                user_product_price = product[user_class][1][user_choice][1]
-                salary -= user_product_price
-                if user_product in shopping_cart.keys():
-                    value = shopping_cart.get(user_product);
-                    value[1] += 1
+                if user_choice < len(product[user_class][1]):
+                    user_product = product[user_class][1][user_choice][0]
+                    user_product_price = product[user_class][1][user_choice][1]
+                    buy_num = input("How many do you want to buy:")
+                    if not buy_num.isdigit():
+                        print("Your have input invalid type...")
+                    else:
+                        buy_num = int(buy_num)
+                        if buy_num*user_product_price > salary:
+                            print("Your balance is not enough [%s] need [%s]" %
+                                  (salary, buy_num*user_product_price))
+                        else:
+                            salary -= user_product_price*buy_num
+                            if user_product in shopping_cart.keys():
+                                value = shopping_cart.get(user_product);
+                                value[1] += buy_num
+                            else:
+                                value = [user_product_price, buy_num]
+                                shopping_cart.setdefault(user_product, value)
+                                print("Your have buy [%s] and balance is [%s]" % (user_product, salary))
                 else:
-                    value = [user_product_price, 1]
-                    shopping_cart.setdefault(user_product, value)
-                print("Your have buy [%s] and balance is [%s]" % (user_product, salary))
+                    print("Your input wrong number...")
             else:
                 print("Your shopping cart is as below".center(50,'*'))
                 print("id   p_name     num    total_price")
@@ -62,7 +77,25 @@ while not exit_flag:
                     p_sum = count*value[0]
                     print(index,'. ',key,"  ",count, "  ", p_sum)
     elif user_class == 'c' or user_class == 'check':
+        print("Your shopping cart is as below".center(50, '*'))
+        print("id   p_name     num    total_price")
+        for item in enumerate(shopping_cart):
+            index = item[0]
+            key = item[1]
+            value = shopping_cart[key]
+            count = value[1]
+            p_sum = count * value[0]
+            print(index, '. ', key, "  ", count, "  ", p_sum)
         print("Your balance is [%s]" % salary)
     elif user_class == 'q' or user_class == "quit":
+        print("Your shopping cart is as below".center(50, '*'))
+        print("id   p_name     num    total_price")
+        for item in enumerate(shopping_cart):
+            index = item[0]
+            key = item[1]
+            value = shopping_cart[key]
+            count = value[1]
+            p_sum = count * value[0]
+            print(index, '. ', key, "  ", count, "  ", p_sum)
         exit_flag = True
 
